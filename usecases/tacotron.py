9abea0622e4eab682a40257ca6ee551df5b594e1
Mohannad <m.barakat@nu.edu.eg>
2021-01-15 13:17:41 +0200

    autopep8 applyed

--- usecases/tacotron.py
+++ usecases/tacotron.py
@@ -1,18 +1,16 @@
+"""
+Example for tacotron pipeline
+"""
 # import ../
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# data load imports
-from data.load.librispeech import load # pylint: disable=imports
-from data.load.libri_what_to_download import what_to_download as wtd # pylint: disable=imports
-from data.load import safe_load # pylint: disable=imports
-
+import pipeline.librispeech as pipeline  # pylint: disable=imports
 # hprams
-from hprams.main import hprams  # pylint: disable=imports 
-
-#  pipeline imports
-import pipeline.librispeech as pipeline # pylint: disable=imports 
-
+from hprams.main import hprams  # pylint: disable=imports
+from data.load import safe_load  # pylint: disable=imports
+from data.load.libri_what_to_download import what_to_download as wtd  # pylint: disable=imports
+from data.load.librispeech import load  # pylint: disable=imports
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 print(os.path.dirname(os.path.abspath(__file__)))
 
--- usecases/test.py
+++ usecases/test.py
@@ -1,17 +1,15 @@
+"""
+Example for wavenet pipeline
+"""
 # import ../
-import os, sys
+import pipeline.librispeech as pipeline  # pylint: disable=imports
+from hprams.test import hprams  # pylint: disable=imports
+from data.load import safe_load  # pylint: disable=imports
+from data.load.libri_what_to_download import what_to_download as wtd  # pylint: disable=imports
+from data.load.librispeech import load  # pylint: disable=imports
+import os
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# data load imports
-from data.load.librispeech import load # pylint: disable=imports
-from data.load.libri_what_to_download import what_to_download as wtd # pylint: disable=imports
-from data.load import safe_load # pylint: disable=imports
-
-# hprams
-from hprams.test import hprams  # pylint: disable=imports 
-
-#  pipeline imports
-import pipeline.librispeech as pipeline # pylint: disable=imports 
 
 
 print(os.path.dirname(os.path.abspath(__file__)))
